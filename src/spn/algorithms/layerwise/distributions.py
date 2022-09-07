wip on variance computation with the closed form: disabled dropout at leaves, fixed classification uncertainties computation, TOFIX numerical issues when computing the classification uncertanties ie the variance associated to the conditionals over the labels given the data -- obtained by applying the Bayes rule

src/spn/algorithms/layerwise/distributions.py
@@ -141,6 +141,7 @@ class Leaf(AbstractLayer):
 
     def _apply_dropout(self, x: torch.Tensor, test_dropout=False, dropout_inference=0.0) -> torch.Tensor:
         # Apply dropout sampled from a bernoulli during training (model.train() has been called)
+        # breakpoint()
 
         if self.dropout > 0.0 and self.training:
             dropout_indices = self._bernoulli_dist.sample(x.shape).bool()
@@ -148,15 +149,17 @@ class Leaf(AbstractLayer):
             # print((x == 0.0).sum())
             # print(dropout_indices.sum())
             # assert(dropout_indices.sum() <=  (x == 0.0).sum())
-        if test_dropout and dropout_inference > 0.0:
-            dropout_indices = torch.distributions.Bernoulli(probs=dropout_inference).sample(x.shape).bool() #NOTE not the most efficient way
-            x[dropout_indices] = 0.0
-            # x[dropout_indices] = np.NINF # TODO test by "truncating" the leaf distribution
-            # print(dropout_indices.sum())
-            # assert (dropout_indices.sum() <= (x == 0.0).sum())
-            # if dropout_cf:
-            #     vars = torch.zeros(x.shape[0]).log()
-            #     return x, vars
+        # apply Monte Carlo dropout (at inference) time
+        # TODO we disable it for now, we apply it only at inner sum nodes
+        # if test_dropout and dropout_inference > 0.0:
+        #     dropout_indices = torch.distributions.Bernoulli(probs=dropout_inference).sample(x.shape).bool() #NOTE not the most efficient way
+        #     x[dropout_indices] = 0.0
+        #     # x[dropout_indices] = np.NINF # TODO test by "truncating" the leaf distribution
+        #     # print(dropout_indices.sum())
+        #     # assert (dropout_indices.sum() <= (x == 0.0).sum())
+        #     # if dropout_cf:
+        #     #     vars = torch.zeros(x.shape[0]).log()
+        #     #     return x, vars
         return x
 
     def _marginalize_input(self, x: torch.Tensor) -> torch.Tensor:

src/spn/algorithms/layerwise/layers.py
@@ -153,30 +153,26 @@ class Sum(AbstractLayer):
         assert log_probs.size() == (n, d, oc, r)
 
         if vars is not None:
-            squared_weights = logweights * 2
-            squared_exps = x * 2
-            input_vars = vars.unsqueeze(3)
-
-            right_term = torch.logsumexp((squared_weights + squared_exps), dim=2)
-
-            # if the sum node corresponds to a root node
-            # if not dropout_cf:
-            #     print("double check here, what's happening with the reshaping")
-            #     breakpoint()
-            #     input_vars = input_vars.reshape((input_vars.shape[0], 1, input_vars.shape[2] * input_vars.shape[4], 1, 1))
-
-            left_term = torch.logsumexp((squared_weights + input_vars), dim=2)
-
             if dropout_cf:
+                squared_weights = logweights * 2
+                squared_exps = x * 2
+                input_vars = vars.unsqueeze(3)
+
+                right_term = torch.logsumexp((squared_weights + squared_exps), dim=2)
+                left_term = torch.logsumexp((squared_weights + input_vars), dim=2)
+
                 right_term += torch.log(torch.tensor(dropout_inference / (1 - dropout_inference)).to(x.device))
                 left_term += torch.log(torch.tensor(1 / (1 - dropout_inference)).to(x.device))
 
-            log_vars = logsumexp(left_term, right_term)
+                log_vars = logsumexp(left_term, right_term)
 
-            assert log_vars.isnan().sum() == 0, "nan values when propagating from a sum node"
-            assert log_vars.isfinite().sum() > 0, "no finite values while propagating from a sum node"
+                assert log_vars.isnan().sum() == 0, "nan values when propagating from a sum node"
+                assert log_vars.isfinite().sum() > 0, "no finite values while propagating from a sum node"
 
-            return log_probs, log_vars
+                return log_probs, log_vars
+            else:
+                # if the sum node corresponds to a root node
+                return log_probs, torch.logsumexp((logweights * 2 + vars.unsqueeze(3)), dim=2)
 
         return log_probs
 

src/spn/experiments/RandomSPNs_layerwise/rat_spn.py
@@ -16,28 +16,31 @@ from icecream import ic
 
 logger = logging.getLogger(__name__)
 
-# def logsumexp(left, right, mask=None):
-#     """
-#     Source: https://github.com/pytorch/pytorch/issues/32097
-#
-#     Logsumexp with custom scalar mask to allow for negative values in the sum.
-#
-#     Args:
-#       tensor:
-#       other:
-#       mask:  (Default value = None)
-#
-#     Returns:
-#
-#     """
-#     if mask is None:
-#         mask = torch.tensor([1, 1])
-#     else:
-#         assert mask.shape == (2,), "Invalid mask shape"
-#
-#     maxes = torch.max(left, right)
-#     breakpoint()
-#     return maxes + ((left - maxes).exp() * mask[0] + (right - maxes).exp() * mask[1]).log()
+def logsumexp(left, right, mask=None):
+    """
+    Source: https://github.com/pytorch/pytorch/issues/32097
+
+    Logsumexp with custom scalar mask to allow for negative values in the sum.
+
+    Args:
+      tensor:
+      other:
+      mask:  (Default value = None)
+
+    Returns:
+
+    """
+    if mask is None:
+        mask = torch.tensor([1, 1])
+    else:
+        assert mask.shape == (2,), "Invalid mask shape"
+
+    maxes = torch.max(left, right)
+    #tt = ((left - maxes).exp() * mask[0] + (right - maxes).exp() * mask[1]).log()
+    # return maxes + torch.where(tt < 0, torch.tensor([0.0]).to(tt.device), tt).log()
+    # return maxes + torch.where(torch.isclose(tt, torch.tensor([0.0]).to(tt.device), atol=1e-03),
+    #                            torch.tensor([0.001]).to(tt.device), tt).log()
+    return maxes + ((left - maxes).exp() * mask[0] + (right - maxes).exp() * mask[1]).log()
 
 def invert_permutation(p: torch.Tensor):
     """(left - maxes).exp() * mask[0] + (right - maxes).exp() * mask[1]
@@ -197,82 +200,102 @@ class RatSpn(nn.Module):
         vars = vars.squeeze(1)
 
         # # compute class probs
-        # vars_copy = vars.clone()
+        vars_copy = vars.detach().clone()
+
+        # we assume class priors are uniform: 1 / n_of_classes #TODO double check if we are learning the root node weights
+        c_i = torch.log(torch.Tensor([1 / self.config.C])).to(x.device)  # 1 / C
+
+        # the variance for each predicted class probabilities decomposes in a product of two terms
+        # here we separate its computation
+        left_term_numerator = x * 2 + c_i * 2
+        left_term_denominator = torch.logsumexp(x + c_i, dim=1) * 2
+        left_term = left_term_numerator - left_term_denominator.reshape((-1, 1)).expand(-1, self.config.C)
+
+        # the right term is composed via the addition/subtraction of 3 terms: a - b + c]
+        # compute a
+        a_numerator = vars + c_i * 2
+        a_denominator = x * 2 + c_i * 2
+        a_term = a_numerator - a_denominator
+
+        # compute b
+        # b corresponds to (in the log space) log(2) + cov - b_denominator
+
+        # # cov is the covariance between two RVs and it is computed via the addition/subtraction
+        # # of 4 terms: d + e + f - g
         #
-        # # we assume class priors are uniform: 1 / n_of_classes #TODO double check if we are learning the root node weights
-        # c_i = torch.log(torch.Tensor([1 / self.config.C])).to(x.device)  # 1 / C
+        # # let's compute the summation over the other elements along the dim 1 excluding the i-th element
+        # x_copy = x.unsqueeze(2).repeat(1, 1, x.shape[1])
+        # x_copy = x_copy + torch.log(1 - torch.eye(x.shape[1])).to(x.device)
+        # x_copy = x_copy.logsumexp(dim=1)
+        # assert x_copy.shape == x.shape
+        # d_term = x + c_i + x_copy + c_i
+        # e_term = vars + c_i * 2
+        # f_term = x * 2 + c_i * 2
+        # g_term = x + c_i + torch.logsumexp(x + c_i, dim=1).reshape((-1, 1)).expand(-1, self.config.C)
         #
-        # # the variance for each predicted class probabilities decomposes in a product of two terms
-        # # here we separate its computation
-        # left_term_numerator = x * 2 + c_i * 2
-        # left_term_denominator = torch.logsumexp(x + c_i, dim=1) * 2
-        # left_term = left_term_numerator - left_term_denominator.reshape((-1, 1)).expand(-1, self.config.C)
+        # # the following cov calculation could cause numerical issues
+        # # cov = torch.log(torch.exp(d_term) + torch.exp(e_term) + torch.exp(f_term) - torch.exp(g_term))
         #
-        # # right term is composed via the addition/subtraction of 3 terms: a - b + c]
-        # # compute a
-        # a_numerator = vars * 2 + c_i * 2
-        # a_denominator = x * 2 + c_i * 2
-        # a_term = a_numerator - a_denominator
-        #
-        # # compute b
-        # # b corresponds to (in the log space) log(2) + cov - b_denominator
-        #
-        # # # cov is the covariance between two RVs and it is computed via the addition/subtraction
-        # # # of 4 terms: d + e + f - g
-        # #
-        # # # let's compute the summation over the other elements along the dim 1 excluding the i-th element
-        # # x_copy = x.unsqueeze(2).repeat(1, 1, x.shape[1])
-        # # x_copy = x_copy + torch.log(1 - torch.eye(x.shape[1])).to(x.device)
-        # # x_copy = x_copy.logsumexp(dim=1)
-        # # assert x_copy.shape == x.shape
-        # # d_term = x + c_i + x_copy + c_i
-        # # e_term = vars + c_i * 2
-        # # f_term = x * 2 + c_i * 2
-        # # g_term = x + c_i + torch.logsumexp(x + c_i, dim=1).reshape((-1, 1)).expand(-1, self.config.C)
-        # #
-        # # # the following cov calculation could cause numerical issues
-        # # # cov = torch.log(torch.exp(d_term) + torch.exp(e_term) + torch.exp(f_term) - torch.exp(g_term))
-        # #
-        # # # manage neg sign in the addition of terms
-        # # mask = torch.tensor([1, -1]).to(x.device)
-        # # cov_left = torch.logsumexp(torch.stack([d_term, e_term, f_term]), dim=0)
-        # # cov = logsumexp(cov_left, g_term, mask=mask)
-        #
-        # cov = vars + c_i * 2
-        #
-        # b_denominator = x + c_i + torch.logsumexp(x, dim=1).reshape((-1, 1)).expand(-1, self.config.C) + c_i
-        # b_term = torch.log(torch.Tensor([2])).to(x.device) + cov - b_denominator
-        #
-        # # compute c
-        # c_numerator = torch.logsumexp(vars + c_i * 2, dim=1)
-        # c_denominator = torch.logsumexp(x * 2 + c_i * 2, dim=1)
-        # c_term = c_numerator - c_denominator
-        #
-        # # the following right_term calculation could cause numerical issues
-        # # right_term = torch.log(torch.exp(a_term) - torch.exp(b_term) +
-        # #                        torch.exp(c_term.reshape((-1, 1)).expand(-1, self.config.C)))
-        # right_term = torch.stack([a_term, b_term, c_term.reshape((-1, 1)).expand(-1, self.config.C)])
+        # # manage neg sign in the addition of terms
+        # mask = torch.tensor([1, -1]).to(x.device)
+        # cov_left = torch.logsumexp(torch.stack([d_term, e_term, f_term]), dim=0)
+        # cov = logsumexp(cov_left, g_term, mask=mask)
+
+        cov = vars + c_i * 2  # this also corresponds to the b term numerator
+
+        b_denominator = x + c_i + torch.logsumexp(x, dim=1).reshape((-1, 1)).expand(-1, self.config.C) + c_i
+        b_term = torch.log(torch.Tensor([2])).to(x.device) + cov - b_denominator
+
+        # compute c
+        c_numerator = torch.logsumexp(vars + c_i * 2, dim=1)
+        c_denominator = torch.logsumexp(x * 2 + c_i * 2, dim=1)
+        c_term = c_numerator - c_denominator
+
+        # the following right_term calculation could cause numerical issues
+        # right_term = torch.log(torch.exp(a_term) - torch.exp(b_term) +
+        #                        torch.exp(c_term.reshape((-1, 1)).expand(-1, self.config.C)))
+        mask = torch.tensor([1, -1]).to(x.device)
+        # right_term = logsumexp(a_term, b_term, mask=mask)
+        # right_term = torch.stack([right_term, c_term.reshape((-1, 1)).expand(-1, self.config.C)])
         # right_term = torch.logsumexp(right_term, dim=0)
-        #
-        # vars = left_term + right_term
-        #
-        # # compute the second order Taylor expansion for the expectations
-        # # the calculation involves three terms h - i + l
-        # # compute h
-        # h_numerator = x + c_i
-        # h_denominator = torch.logsumexp(x, dim=1).reshape((-1, 1)).expand(-1, self.config.C) + c_i
-        # h_term = h_numerator - h_denominator
-        #
-        # # compute i:
-        # i_term = cov - h_denominator * 2
-        #
-        # # compute l
-        # l_numerator = x + c_i + torch.logsumexp(vars_copy, dim=1).reshape((-1, 1)).expand(-1, self.config.C) + c_i * 2
-        # l_denominator = h_denominator * 3
-        # l_term = l_numerator - l_denominator
-        #
-        # # update x with its Taylor expansion
-        # # x = h_term - i_term + l_term
+
+        right_term_1 = logsumexp(a_term, c_term.reshape((-1, 1)).expand(-1, self.config.C),
+                                 mask=torch.tensor([1, 1]).to(a_term.device))
+        right_term = logsumexp(right_term_1, b_term, mask=torch.tensor([1, -1]).to(a_term.device))
+        # TODO the problem here is that we don't always know the source of the nan value
+        # if it is always because of a small negative value in the decimal space then could be ok
+        # to apply the following approximation (usually this small negative values come from numerical
+        # approximation issue, from difference of (almost) equal numbers that should have 0 as outcome).
+        right_term = torch.where(right_term.isnan(), torch.tensor([-float('inf')]).to(right_term.device), right_term) # TODO
+
+        vars = left_term + right_term
+
+        # compute the second order Taylor expansion for the expectations
+        # the calculation involves three terms h - i + l
+        # compute h
+        h_numerator = x + c_i
+        h_denominator = torch.logsumexp(x, dim=1).reshape((-1, 1)).expand(-1, self.config.C) + c_i
+        h_term = h_numerator - h_denominator
+
+        # compute i:
+        i_term = cov - h_denominator * 2
+
+        # compute l
+        l_numerator = x + c_i + torch.logsumexp(vars_copy, dim=1).reshape((-1, 1)).expand(-1, self.config.C) + c_i * 2
+        l_denominator = h_denominator * 3
+        l_term = l_numerator - l_denominator
+
+        # update x with its Taylor expansion
+        # x = logsumexp(logsumexp(h_term, i_term, mask=mask), l_term, mask=torch.tensor([1, 1]).to(x.device))
+
+        # TODO here too, we need to manage negative values result of the difference otherwise
+        # would be impossible to operate in the log space
+        x = logsumexp(logsumexp(h_term, l_term), i_term, mask=mask)
+
+        # assert x.isfinite().sum() == torch.prod(torch.tensor(x.shape)), breakpoint()
+        # assert vars.isfinite().sum() == torch.prod(torch.tensor(vars.shape)), breakpoint()
+        assert x.isnan().sum() == 0, breakpoint()
+        assert vars.isnan().sum() == 0, breakpoint()
 
         return x, vars
 

src/spn/experiments/RandomSPNs_layerwise/train_mnist.py
@@ -875,6 +875,7 @@ def evaluate_corrupted_svhn(model_dir=None, dropout_inference=None, n_mcd_passes
 
 def test_closed_form(model_dir=None, training_dataset=None, dropout_inference=None, batch_size=20,
                   rat_S=20, rat_I=20, rat_D=5, rat_R=5, rotation=None):
+    ic(rotation)
     # dev = sys.argv[1]
     # device = torch.device("cuda:0")
     device = sys.argv[1]
@@ -901,6 +902,7 @@ def test_closed_form(model_dir=None, training_dataset=None, dropout_inference=No
     # old models
     # model.load_state_dict(torch.load(model_dir + 'model.pt'))
     model.eval()
+    print(model)
 
     tag = "Testing Closed Form dropout: "
 
@@ -921,9 +923,14 @@ def test_closed_form(model_dir=None, training_dataset=None, dropout_inference=No
         criterion = nn.CrossEntropyLoss(reduction="sum")
     # criterion = nn.NLLLoss(reduction="sum")
 
+    # max_std_eq_max_conds = 0
+
+
+
     with torch.no_grad():
         for batch_index, (data, target) in enumerate(test_loader):
             data, target = data.to(device), target.to(device)
+            ic()
 
             if rotation:
                 data = torchvision.transforms.functional.rotate(data.reshape(-1, 1, 28, 28), rotation,
@@ -933,22 +940,28 @@ def test_closed_form(model_dir=None, training_dataset=None, dropout_inference=No
 
             output, stds = model(data, test_dropout=True, dropout_inference=dropout_inference, dropout_cf=True)
             # output = model(data, test_dropout=False, dropout_inference=0.0, dropout_cf=False)
+            # ic(output.exp())
             # ic(output)
             # ic(stds)
             # ic((output - torch.logsumexp(output, dim=1, keepdims=True)).exp())
-            ic(output.max(dim=1))
+            # ic(output.max(dim=1))
+            # ic(output.max(dim=1)[0].exp())
             # ic(output.argmax(dim=1))
             # ic(stds.argmin(dim=1))
             # ic(stds.argmax(dim=1))
             # ic(target)
             # ic(stds)
-            # ic(stds.exp())
-            # ic(stds.gather(1, output.argmax(dim=1).view(-1,1)))
-            # ic(stds.gather(1, output.argmax(dim=1).view(-1, 1)).exp())
+            # # ic(stds.exp())
+            # # ic(stds.gather(1, output.argmax(dim=1).view(-1,1)))
+            # # ic(stds.gather(1, output.argmax(dim=1).view(-1, 1)).exp())
             # ic(stds.gather(1, output.argmax(dim=1).view(-1, 1)).mean())
             # ic(stds.mean())
-            breakpoint()
-            assert stds.isfinite().sum() == torch.prod(torch.tensor(stds.shape)), "there is at least a non-finite value"
+            # breakpoint()
+            # max_std_eq_max_conds += (output.argmax(dim=1) == stds.argmax(dim=1)).sum().item()
+            # ic(torch.where(output.max(dim=1)[0].exp().isnan(), torch.tensor([0.0]).to(output.device), output.max(dim=1)[0].exp()).mean())
+            # ic((torch.where(stds.isnan(), torch.tensor([0.0]).to(stds.device), stds).sum(dim=1)/9.0).mean())
+            # assert stds.isfinite().sum() == torch.prod(torch.tensor(stds.shape)), "there is at least a non-finite value"
+            # assert output.isfinite().sum() == torch.prod(torch.tensor(output.shape)), breakpoint()
 
             # sum up batch loss
             if model.config.C == 2:
@@ -967,6 +980,8 @@ def test_closed_form(model_dir=None, training_dataset=None, dropout_inference=No
             loss_nll += -output.sum()
             pred = output.argmax(dim=1)
             correct += (pred == target).sum().item()
+
+        # ic(max_std_eq_max_conds)
 
     loss_ce /= len(test_loader.dataset)
     loss_nll /= len(test_loader.dataset) + get_data_flatten_shape(test_loader)[1]
@@ -2402,8 +2417,10 @@ if __name__ == "__main__":
     # test_closed_form(model_dir='results/2022-08-29_13-50-24/model/', training_dataset='mnist', dropout_inference=0.01)
     # test_closed_form(model_dir='results/2022-08-29_13-50-24/model/', training_dataset='mnist', dropout_inference=0.05)
     # test_closed_form(model_dir='results/2022-08-29_13-50-24/model/', training_dataset='mnist', dropout_inference=0.1)
-    test_closed_form(model_dir='results/2022-08-29_13-50-24/model/', training_dataset='mnist', dropout_inference=0.1,
-                     batch_size=20, rotation=90)
+    test_closed_form(model_dir='results/2022-08-29_13-50-24/model/', training_dataset='mnist', dropout_inference=0.2,
+                     batch_size=500, rotation=None)
+    # test_closed_form(model_dir='results/2022-08-29_13-50-24/model/', training_dataset='mnist', dropout_inference=0.2,
+    #                  batch_size=500, rotation=None)
     # test_closed_form(model_dir='results/2022-08-29_13-50-24/model/', training_dataset='mnist', dropout_inference=0.2)
     # test_closed_form(model_dir='results/2022-08-29_13-50-24/model/', training_dataset='mnist', dropout_inference=0.2)
     # test_closed_form(model_dir='results/2022-08-29_13-50-24/model/', training_dataset='mnist', dropout_inference=0.2)